a31eea74cd7710351957d6ed83dd3cf7125078b6
Tom Bredemeier <tbredemeier@barrington220.org>
2021-02-18 14:12:31 -0600

    Finished setting up alert

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var jokes = [Joke]()
+    @State private var showingAlert = false
     var body: some View {
         NavigationView {
             List(jokes) { joke in
@@ -23,6 +24,11 @@ struct ContentView: View {
         }
         .onAppear(perform: {
             getJokes()
+        })
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the data"),
+                  dismissButton: .default(Text("OK")))
         })
     }
     
@@ -40,9 +46,11 @@ struct ContentView: View {
                         let joke = Joke(setup: setup, punchline: punchline)
                         jokes.append(joke)
                     }
+                    return
                 }
             }
         }
+        showingAlert = true
     }
 }
 
