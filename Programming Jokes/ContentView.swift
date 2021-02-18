f0c2c238d10aad4246ab97180641c49dcb76be1e
Tom Bredemeier <tbredemeier@barrington220.org>
2021-02-18 09:36:45 -0600

    Got API key

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -27,12 +27,7 @@ struct ContentView: View {
     }
     
     func getJokes() {
-        jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
-                          punchline: "Because DEC 25 = OCT 31"))
-        jokes.append(Joke(setup: "How did the programmer die in the shower?",
-                          punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-        jokes.append(Joke(setup: "There are 10 types of people in the world",
-                          punchline: "Those who understand binary and those who don’t."))
+        let apiKey = "d87dc96880msh138dad116ed364ep17b762jsnfa65120c7d18"
     }
 }
 
