627cc5fba33a96e74667f2594096341c33d8325c
Tom Bredemeier <tbredemeier@barrington220.org>
2021-02-18 10:05:15 -0600

    Set up API query

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -27,7 +27,22 @@ struct ContentView: View {
     }
     
     func getJokes() {
-        let apiKey = "d87dc96880msh138dad116ed364ep17b762jsnfa65120c7d18"
+        let apiKey = "?rapidapi-key=d87dc96880msh138dad116ed364ep17b762jsnfa65120c7d18"
+        let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["success"] == true {
+                    let contents = json["body"].arrayValue
+                    for item in contents {
+                        let setup = item["setup"].stringValue
+                        let punchline = item["punchline"].stringValue
+                        let joke = Joke(setup: setup, punchline: punchline)
+                        jokes.append(joke)
+                    }
+                }
+            }
+        }
     }
 }
 
