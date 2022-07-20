fd97e995f4a314f6f0be380c5724da2f928a11ee
Tom Bredemeier <tbredemeier@barrington220.org>
2022-07-20 18:55:00 -0500

    Initial Commit

---
+++ Programming Jokes/Programming_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_JokesApp.swift
+//  Programming Jokes
+//
+//  Created by Tom Bredemeier on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Programming_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
